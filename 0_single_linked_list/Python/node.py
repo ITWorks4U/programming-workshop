e31ff93ff93ab669480ce736268fa4076fc7e900
ITWorks4U <itworks4U@gmail.com>
2025-07-22 10:09:19 +0200

    updated linked-list-limitations

--- 0_single_linked_list/Python/node.py
+++ 0_single_linked_list/Python/node.py
@@ -2,9 +2,14 @@
 #
 #	author:		ITWorks4U
 #	created:	July 14th, 2025
+#	updated:	July 22nd, 2025
 #
 
 class Node:
+	#NOTE:
+	#	usually, there's no need to check for a length
+	#	of content, however, similar to C programming
+	#	a limitation of 128 characters shall be used here as well
 	_MAX_INPUT_LENGTH = 128
 	_INVALID_INPUT = "*** content length was too long"
 
@@ -24,7 +29,7 @@ class Node:
 		"""
 		self.node_id = node_id
 
-		if not self._on_valid_content_length(content_to_insert=content):
+		if len(content) >= self._MAX_INPUT_LENGTH:
 			self.content = self._INVALID_INPUT
 		else:
 			self.content = content
@@ -32,21 +37,4 @@ class Node:
 
 		self.next = None
 	#end constructor
-
-	def _on_valid_content_length(self, content_to_insert: str) -> bool:
-		"""
-		Checks, if the content length is valid. The length of 127 characters
-		must not exceed.
-
-		content_to_insert:
-
-		-	the content for the new node
-
-		returns:
-
-		-	True, if the length of the new node's content does not exceed 127 characters
-		-	False, otherwise
-		"""
-		return len(content_to_insert) < self._MAX_INPUT_LENGTH
-	#end method
-#end class node
+#end class
